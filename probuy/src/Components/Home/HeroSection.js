1e97695f33220a4e92586127be0c652cc4b40ffd
Sujoy Paul <freelancersujoy@gmail.com>
2023-06-19 21:47:21 +0600

    hero section completed

--- probuy/src/Components/Home/HeroSection.js
+++ probuy/src/Components/Home/HeroSection.js
@@ -1,22 +1,35 @@
 
-import React from 'react';
+import React, { useState } from 'react';
 import chair from '../../assets/sofa.jpg'
 import Image from 'next/image'
 import ContainItem from '../../Commons/Container';
 
 const HeroSection = () => {
+
+  const [focusButton, setFocusButton] = useState(true)
+
     return (
         <div className='pt-20'>
             <ContainItem>
-                <div className='flex '>
+                <div className='flex w-[1240px]'>
                   <div className='w-[55%] flex items-center'>
                     <div>
-                        <h2 className='text-5xl lowercase leading-[60px]'><span className='capitalize'>Prepare</span> to have Your mind blown by the most mind-bending <span className='text-indigo-500'>ProBuy Product</span> of all time!</h2>
+                        <h2 className='text-5xl lowercase leading-[60px] select-none'><span className='capitalize'>Prepare</span> to have Your mind <span className='text-4xl'>blown by the most mind bending  <span className='text-indigo-500 capitalize font-[600]'>ProBuy Product</span> of all time!</span> </h2>
                         <p className='text-xl mt-5'>Prepare to have your mind blown as you delve into the astonishing realm of the most mind-bending ProBuy product ever created. This innovative marvel will redefine your perception of what's possible. </p>
+
+                        {/* action items  */}
+                        <div className='mt-8 flex gap-x-3'>
+                            <button 
+                            onMouseEnter={() => setFocusButton(false)} 
+                            onMouseLeave={() => setFocusButton(true)}
+                            className={`text-md bg-indigo-500 text-white py-3 px-10 rounded-md hover:bg-white hover:text-indigo-500 border-2  border-indigo-500 select-none duration-500 ${focusButton ? 'btnShadow' : 'btnShadow2'}`}>Getting started</button>
+
+                            <button className='text-md hover:bg-black hover:text-white py-3 px-10 rounded-md bg-white text-indigo-500 border-2  border-indigo-500 hover:border-black duration-500 select-none'>Shop now</button>
+                        </div>
                     </div>
                   </div>
                   <div className='w-[45%] flex items-center justify-center h-[10%]'>
-                    <Image className='w-[100%]' src={chair}></Image>
+                    <Image className='w-[100%] select-none' src={chair}></Image>
                   </div>
                 </div>
             </ContainItem>
--- probuy/src/Components/Home/Home.js
+++ probuy/src/Components/Home/Home.js
@@ -2,6 +2,7 @@ import React from 'react'
 import HeroSection from './HeroSection'
 
 export default function Home() {
+  
   return (
     <div className='flex justify-center items-center h-[90vh]'>
       <HeroSection></HeroSection>
